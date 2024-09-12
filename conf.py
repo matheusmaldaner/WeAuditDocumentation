9bee6e56f09d3c36adf7f807448643c509453c46
Matheus Maldaner <matheus@sisnema.com.br>
2024-09-12 17:53:48 -0400

    Add documentation files

--- conf.py
+++ conf.py
@@ -14,7 +14,7 @@ release = '2024'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.githubpages']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'sphinx_env', 'Lib', 'include']
